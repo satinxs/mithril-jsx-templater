ed7ab94d28c304979d155d6170241fba48670508
satinxs <satinxs@gmail.com>
2017-06-28 15:17:57 -0300

    Fixed getTemplate bug, made several changes to file structure.

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 // use a mock DOM so we can run mithril on the server
 require('mithril/test-utils/browserMock')(global);
 
-const
-  assert = require('assert'),
-  fs = require('fs'),
-  path = require('path'),
-  babel = require("babel-core"),
-  mithril = require('mithril'),
-  render = require('mithril-node-render');
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const { transform } = require("babel-core");
+const mithril = require('mithril');
+const render = require('mithril-node-render');
 
 function BabelTransform(source) {
-  return babel.transform(source, { "plugins": [["transform-react-jsx", { "pragma": "h" }]] });
+  return transform(source, { "plugins": [["transform-react-jsx", { "pragma": "h" }]] });
 };
 
+function BootstrapSource(source) {
+  return "(function(h, sx) { return async function(it) {  \n return " + source + ";\n }; })";
+}
+
 module.exports = class Templater {
-  constructor(viewsPath) {
+  constructor(viewsPath, debug) {
     assert(viewsPath, "The Views path is obligatory!");
     this.viewsPath = viewsPath;
     this.templates = [];
+    this.debug = debug || false;
   }
 
   async compile(templateName) {
-    let templatePath = path.join(this.viewsPath, templateName + '.jsx');
+    try {
+      let templatePath = path.join(this.viewsPath, templateName + '.jsx');
 
-    if (!fs.existsSync(templatePath)) throw new Error('File does not exist');
+      if (!fs.existsSync(templatePath)) throw new Error('File does not exist');
 
-    let file = fs.readFileSync(templatePath).toString();
+      let file = fs.readFileSync(templatePath).toString();
 
-    file = "(function(h, sx) { return async function(it) { \n " + file + "\n }; })";
+      file = BootstrapSource(file);
 
-    let compiled = BabelTransform(file).code;
+      let compiled = BabelTransform(file).code;
 
-    this.templates[templateName] = eval(compiled)(mithril, this);
+      this.templates[templateName] = eval(compiled)(mithril, this);
+
+    } catch (e) {
+      throw new Error('Error compiling template "' + templateName + '"\nError: ' + e.message);
+    }
   }
 
   async render(templateName, model) {
@@ -43,12 +52,13 @@ module.exports = class Templater {
   }
 
   async executeTemplate(templateName, model) {
-    return await this.getTemplate(templateName)(model);
+    return await (await this.getTemplate(templateName))(model);
   }
 
-  getTemplate(templateName) {
-    if (!this.templates[templateName])
-      this.compile(templateName);
+  async getTemplate(templateName) {
+
+    if (!this.templates[templateName] || this.debug)
+      await this.compile(templateName);
 
     return this.templates[templateName];
   }
