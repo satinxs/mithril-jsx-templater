2bcb793735a633e897e657012001bc6bb37bf3e2
satinxs <satinxs@gmail.com>
2017-06-28 10:36:28 -0300

    Changed the structure of the module to ES6 class

--- index.js
+++ index.js
@@ -13,53 +13,43 @@ function BabelTransform(source) {
   return babel.transform(source, { "plugins": [["transform-react-jsx", { "pragma": "h" }]] });
 };
 
-const Templater = function (viewsPath) {
-
-  assert(viewsPath, "The Views path is obligatory!");
-
-  this.viewsPath = viewsPath;
-
-  this.templates = [];
-
-  this.compile = async (templateName) => {
-    try {
-      let templatePath = path.join(viewsPath, templateName + '.jsx');
-
-      if (!fs.existsSync(templatePath)) throw new Error('File does not exist');
-
-      let file = fs.readFileSync(templatePath).toString();
-
-      file = "(function(h, sx) { return async function(it) { \n " + file + "\n }; })";
-
-      let compiled = BabelTransform(file).code;
-
-      this.templates[templateName] = eval(compiled)(mithril, this);
-
-    } catch (e) {
-      throw e;
-    }
+module.exports = class Templater {
+  constructor(viewsPath) {
+    assert(viewsPath, "The Views path is obligatory!");
+    this.viewsPath = viewsPath;
+    this.templates = [];
   }
 
-  this.render = async function (templateName, model) {
+  async compile(templateName) {
+    let templatePath = path.join(this.viewsPath, templateName + '.jsx');
+
+    if (!fs.existsSync(templatePath)) throw new Error('File does not exist');
+
+    let file = fs.readFileSync(templatePath).toString();
+
+    file = "(function(h, sx) { return async function(it) { \n " + file + "\n }; })";
+
+    let compiled = BabelTransform(file).code;
+
+    this.templates[templateName] = eval(compiled)(mithril, this);
+  }
+
+  async render(templateName, model) {
     return await render(await this.executeTemplate(templateName, model));
-  };
+  }
 
-  this.extends = async function (templateBaseName, props) {
+  async extends(templateBaseName, props) {
     return await this.executeTemplate(templateBaseName, props);
-  };
+  }
 
-  this.executeTemplate = async function (templateName, model) {
+  async executeTemplate(templateName, model) {
     return await this.getTemplate(templateName)(model);
-  };
+  }
 
-  this.getTemplate = function (templateName) {
+  getTemplate(templateName) {
     if (!this.templates[templateName])
       this.compile(templateName);
 
     return this.templates[templateName];
-  };
-
-  return this;
+  }
 }
-
-module.exports = Templater;
